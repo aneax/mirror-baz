5a0438a7538061a171fcb6b80ec5fb343ac44cfb
aneax <acharyanish7@gmail.com>
2023-05-09 22:14:59 +0200

    from mirror: update cmakelists and baz

--- baz.cpp
+++ baz.cpp
@@ -1,3 +1,5 @@
 #include "baz.hpp"
-
-auto call_baz() -> std::string { return "bazbaz"; }
+#include <string_view>
+#include <string>
+static constexpr std::string_view Baz = "bazbaz";
+auto call_baz() -> std::string { return std::string(Baz); }
